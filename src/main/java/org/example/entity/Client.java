3d8709334d453c21a6fb58967cf826e3ab4490cd
smagles <lesialist@icloud.com>
2023-10-23 19:21:22 +0200

    Added Client and Planet entities

---
+++ src/main/java/org/example/entity/Client.java
@@ -0,0 +1,18 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "client")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", length = 200, nullable = false)
+    private String name;
+
+}
---
+++ src/main/java/org/example/entity/Planet.java
@@ -0,0 +1,20 @@
+package org.example.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "planet")
+public class Planet {
+    @Id
+    @Column(name = "id", length = 50, unique = true, nullable = false)
+    private String id;
+
+    @Column(name = "name", length = 500, nullable = false)
+    private String name;
+
+}
