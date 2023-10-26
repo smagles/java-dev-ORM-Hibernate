25e7f5f74fcea36101fb7edb61b0a323d2683236
smagles <lesialist@icloud.com>
2023-10-27 00:31:54 +0200

    performed database migrations, client and planet CRUD operations

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+
+import org.example.entity.Client;
+import org.example.entity.Planet;
+import org.example.flyway.DatabaseMigration;
+import org.example.service.ClientCrudService;
+import org.example.service.PlanetCrudService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DatabaseMigration databaseMigration = new DatabaseMigration();
+        databaseMigration.fwMigration();
+
+        ClientCrudService clientCrudService = new ClientCrudService();
+        Client newClient = new Client();
+        newClient.setName("Thanos");
+        clientCrudService.create(newClient);
+        clientCrudService.delete(newClient);
+
+        PlanetCrudService planetCrudService = new PlanetCrudService();
+        Planet newPlanet = new Planet();
+        newPlanet.setId("TITAN");
+        newPlanet.setName("Titan");
+        planetCrudService.create(newPlanet);
+        planetCrudService.delete(newPlanet);
     }
 }
