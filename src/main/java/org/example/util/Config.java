63c97f823d1095940bc757c97b80bf67481ce9bd
smagles <lesialist@icloud.com>
2023-10-27 00:28:28 +0200

    utils

---
+++ src/main/java/org/example/util/Config.java
@@ -0,0 +1,7 @@
+package org.example.util;
+
+public class Config {
+    public static final String URL = "jdbc:h2:./mydb";
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+}
