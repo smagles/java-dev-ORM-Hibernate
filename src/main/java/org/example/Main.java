ae8321ce890072d187291aa8b14f408c01f45489
smagles <lesialist@icloud.com>
2023-10-29 15:46:39 +0100

    Implemented validation for ticket creation against non-existing or null client and planet.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,13 @@ package org.example;
 
 import org.example.entity.Client;
 import org.example.entity.Planet;
+import org.example.entity.Ticket;
 import org.example.flyway.DatabaseMigration;
 import org.example.service.ClientCrudService;
 import org.example.service.PlanetCrudService;
+import org.example.service.TicketCrudService;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,5 +28,15 @@ public class Main {
         newPlanet.setName("Titan");
         planetCrudService.create(newPlanet);
         planetCrudService.delete(newPlanet);
+
+        TicketCrudService ticketCrudService = new TicketCrudService();
+        Ticket newTicket = new Ticket();
+        Date currentDate = new Date();
+        newTicket.setCreatedAt(currentDate);
+        newTicket.setClient(null);
+        newTicket.setFromPlanet(null);
+        newTicket.setToPlanet(null);
+        ticketCrudService.create(newTicket);
+
     }
 }
