cf803e488153d19eaa5c064af36fd72d7a237b52
smagles <lesialist@icloud.com>
2023-10-29 14:55:22 +0100

    Modified V1,V2.sql

--- src/main/resources/db/migration/V1__create_db.sql
+++ src/main/resources/db/migration/V1__create_db.sql
@@ -1,20 +1,20 @@
-CREATE TABLE clients (
+create TABLE clients (
     id LONG AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(200) NOT NULL
 );
 
-CREATE TABLE planets (
+create TABLE planets (
     id VARCHAR(50) PRIMARY KEY,
     name VARCHAR(500) NOT NULL
 );
-CREATE TABLE tickets (
+create TABLE tickets (
     id LONG AUTO_INCREMENT PRIMARY KEY,
     created_at TIMESTAMP,
     client_id LONG,
     from_planet_id VARCHAR(50),
     to_planet_id VARCHAR(50),
-    FOREIGN KEY (client_id) REFERENCES client (id),
-    FOREIGN KEY (from_planet_id) REFERENCES planet (id),
-    FOREIGN KEY (to_planet_id) REFERENCES planet (id)
+    FOREIGN KEY (client_id) REFERENCES clients (id),
+    FOREIGN KEY (from_planet_id) REFERENCES planets (id),
+    FOREIGN KEY (to_planet_id) REFERENCES planets (id)
 );
 
