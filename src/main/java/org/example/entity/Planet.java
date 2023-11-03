6b1d92c9aa060f80118301f8f8e38202b3b17383
smagles <lesialist@icloud.com>
2023-11-03 15:01:10 +0100

    Updated Client, Planet, Ticket entity

--- src/main/java/org/example/entity/Planet.java
+++ src/main/java/org/example/entity/Planet.java
@@ -9,7 +9,7 @@ import lombok.Data;
 
 @Entity
 @Data
-@Table(name = "planets")
+@Table(name = "Planets")
 public class Planet {
     @Id
     @Column(name = "id", length = 50, unique = true, nullable = false)
--- src/main/java/org/example/entity/Ticket.java
+++ src/main/java/org/example/entity/Ticket.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 @Entity
 @Data
-@Table(name = "tickets")
+@Table(name = "Tickets")
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
